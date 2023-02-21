578425040f997bcd493f4149817a36de0d853a6b
myogin <yoginugraha19@gmail.com>
2023-02-21 14:50:32 +0800

    auth basic implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,87 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbRegisterComponent,
+  NbLogoutComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+} from '@nebular/auth';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    // canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'prefix',
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./features/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          ),
+      },
+    ],
+  },
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+      {
+        path: 'auth',
+        component: NbAuthComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'login',
+            pathMatch: 'prefix',
+          },
+          {
+            path: 'login',
+            component: NbLoginComponent,
+          },
+          {
+            path: 'register',
+            component: NbRegisterComponent,
+          },
+          {
+            path: 'logout',
+            component: NbLogoutComponent,
+          },
+          {
+            path: 'request-password',
+            component: NbRequestPasswordComponent,
+          },
+          {
+            path: 'reset-password',
+            component: NbResetPasswordComponent,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: 'features/dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: '**',
+    component: DashboardLayoutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
